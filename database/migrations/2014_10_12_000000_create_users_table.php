04af2fbd1347f67072e55a6f55106aef5357bb8d
Raihan S <raihan.act@gmail.com>
2018-10-30 01:31:22 +0600

    migrations for user, tenant

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,24 +15,46 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->uuid('uuid')->nullable()->default(null);
+            $table->string('uuid', 64)->nullable()->default(null);
             $table->unsignedInteger('tenant_id')->nullable()->default(null);
+            $table->string('name', 36)->nullable()->default(null);
 
-            $table->string('name')->nullable()->default(null);
-            $table->string('email')->unique();
-            $table->string('password');
+            /*********************************/
+            /*  Custom columns
+            /*********************************/
+            $table->string('email', 100)->unique();
+            $table->string('password', 100);
+            $table->rememberToken();
+
+            // Email confirmation fields
             $table->boolean('email_confirmed')->default(false);
             $table->dateTime('email_confirmed_at')->nullable()->default(null);
-            $table->string('email_confirmation_code',36)->nullable();
+            $table->string('email_confirmation_code', 128)->nullable();
 
-            $table->string('access_token',36)->nullable()->default(null);
+            // Access token
+            $table->string('access_token', 256)->nullable()->default(null);
             $table->dateTime('access_token_generated_at')->nullable()->default(null);
-            $table->string('api_token',36)->nullable()->default(null);
-            $table->dateTime('access_token_generated_at')->nullable()->default(null);
 
+            // API token
+            $table->string('api_token', 256)->nullable()->default(null);
+            $table->dateTime('api_token_generated_at')->nullable()->default(null);
 
-            $table->rememberToken();
+            // Can tenant edit a row?
+            $table->boolean('tenant_editable')->default(true);
+
+            // Permission and user group
+            $table->string('permissions', 2056)->nullable()->default(null);
+            $table->string('usergroups', 2056)->nullable()->default(null); // Stores object/json
+            $table->string('usergroup_ids_csv', 256)->nullable()->default(null); // 1,2,3
+            $table->string('usergroup_titles_csv', 1024)->nullable()->default(null); // Super admin, Tenant admin
+
+            /*********************************/
+            $table->boolean('is_active')->default(true);
+            $table->unsignedInteger('created_by')->nullable()->default(null);
+            $table->unsignedInteger('updated_by')->nullable()->default(null);
             $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedInteger('deleted_by')->nullable()->default(null);
         });
     }
 
---
+++ database/migrations/2018_10_29_190946_create_tenants_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid', 64)->nullable()->default(null);
+            //$table->unsignedInteger('tenant_id')->nullable()->default(null);
+            $table->string('name', 255)->nullable()->default(null);
+            $table->string('code', 32)->nullable()->default(null);
+
+            /******* Custom columns **********/
+            $table->unsignedInteger('user_id')->nullable()->default(null); // Tenant admin user id.
+            /*********************************/
+
+            $table->boolean('is_active')->default(true);
+            $table->unsignedInteger('created_by')->nullable()->default(null);
+            $table->unsignedInteger('updated_by')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedInteger('deleted_by')->nullable()->default(null);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
